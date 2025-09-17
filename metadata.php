27918209caa5d50ba63c1f0bb6e3e7fb705e4c65
Michael Keiluweit <michael.keiluweit@gmail.com>
2025-09-17 11:52:57 +0200

    replaced module description

--- metadata.php
+++ metadata.php
@@ -10,7 +10,8 @@ $sMetadataVersion = '2.1';
 $aModule = [
     'id' => LoggingFrameworkModule::ID,
     'title' => 'Minimal Invasive Massive Logging',
-    'description' => 'PSR-3 Logging mit Request-Kontext, Error/Exception/Shutdown Hooks',
+    'description' => 'This module provides detailed request logging for OXID eShop, capturing what users do inside the shop.
+It records key request data such as visited pages, parameters, and context, making user flows and issues traceable.',
     'version' => '1.0.0',
     'author' => 'support@oxid-esales.com',
     'extend' => [
