85cf80da5f78611c46e14e3f01caf38423c89efb
OXIDSimonGassenchmidt <simon.gassenschmidt@oxid-esales.com>
2026-01-26 12:27:55 +0100

    add test suite according to example module

---
+++ tests/PhpStan/phpstan-bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+// Create class alias for NavigationController_parent
+class_alias(
+    \OxidEsales\Eshop\Application\Controller\Admin\NavigationController::class,
+    \OxidSupport\Heartbeat\Shared\Controller\Admin\NavigationController_parent::class
+);
